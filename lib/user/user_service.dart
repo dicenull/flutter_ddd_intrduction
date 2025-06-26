e56ce63d9839ff6ddf762fa9c6857089da8a9af8
dicenull <dicenull.dev@gmail.com>
2025-06-26 19:40:20 +0900

    No.1756 ユーザの重複をRepositoryで確認

--- lib/user/user_service.dart
+++ lib/user/user_service.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_ddd_introduction/user/user.dart';
+import 'package:flutter_ddd_introduction/user/user_repository.dart';
 
 class UserService {
+  final IUserRepository _userRepository;
+
+  UserService(this._userRepository);
+
   bool exists(User user) {
-    // TODO: 重複確認を実装
-    return user.hashCode % 2 == 0;
+    // ユーザの重複を問い合わせる
+    return _userRepository.find(user.name) != null;
   }
 }
