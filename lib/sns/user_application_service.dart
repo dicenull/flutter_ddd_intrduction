c2c0b6960641d1a960f5e6bf3882a13304ba9031
dicenull <dicenull.dev@gmail.com>
2025-07-03 19:35:06 +0900

    No.2279 ユーザ更新

--- lib/sns/user_application_service.dart
+++ lib/sns/user_application_service.dart
@@ -30,6 +30,29 @@ class UserApplicationService {
     final userData = UserData(user);
     return userData;
   }
+
+  void update(String userId, String name) {
+    final targetId = UserId(userId);
+    final user = _userRepository.findById(targetId);
+
+    if (user == null) {
+      throw UserNotFoundException('ユーザが見つかりません: $userId');
+    }
+
+    final newUserName = UserName(name);
+    user.changeName(newUserName);
+    if (_userService.exists(user)) {
+      throw CanNotRegisterException('ユーザ名 $name はすでに存在します');
+    }
+
+    _userRepository.save(user);
+  }
+}
+
+class UserNotFoundException implements Exception {
+  final String message;
+
+  UserNotFoundException(this.message);
 }
 
 class CanNotRegisterException implements Exception {
