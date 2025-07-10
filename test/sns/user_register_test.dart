4d5945ad31fa6b7d1b679bbf13cba86dd68dfda4
dicenull <dicenull.dev@gmail.com>
2025-07-10 19:33:47 +0900

    No.3255 ユーザ登録できない場合のテスト

--- test/sns/user_register_test.dart
+++ test/sns/user_register_test.dart
@@ -45,4 +45,63 @@ void main() {
     final user = userRepository.findByName(UserName("12345678901234567890"));
     expect(user, isNotNull);
   });
+
+  test('ユーザ名は最小の文字列未満で登録できない', () {
+    final container = createContainer(
+      overrides: [
+        userRepositoryPod.overrideWithValue(InMemoryUserRepository()),
+      ],
+    );
+
+    final userApplicationService = container.read(userApplicationServicePod);
+    container.listen(userApplicationServicePod, (_, __) {});
+
+    bool exceptionOccurred = false;
+    try {
+      userApplicationService.register("12");
+    } catch (e) {
+      exceptionOccurred = true;
+    }
+    expect(exceptionOccurred, isTrue);
+  });
+
+  test('ユーザ名は最大の文字列を超えて登録できない', () {
+    final container = createContainer(
+      overrides: [
+        userRepositoryPod.overrideWithValue(InMemoryUserRepository()),
+      ],
+    );
+
+    final userApplicationService = container.read(userApplicationServicePod);
+    container.listen(userApplicationServicePod, (_, __) {});
+
+    bool exceptionOccurred = false;
+    try {
+      userApplicationService.register("123456789012345678901");
+    } catch (e) {
+      exceptionOccurred = true;
+    }
+    expect(exceptionOccurred, isTrue);
+  });
+
+  test('重複したユーザ名で登録できない', () {
+    final container = createContainer(
+      overrides: [
+        userRepositoryPod.overrideWithValue(InMemoryUserRepository()),
+      ],
+    );
+    final userRepository = container.read(userRepositoryPod);
+    final userApplicationService = container.read(userApplicationServicePod);
+
+    container.listen(userApplicationServicePod, (_, __) {});
+    userRepository.save(User(UserName("test-user")));
+
+    bool exceptionOccurred = false;
+    try {
+      userApplicationService.register("test-user");
+    } catch (e) {
+      exceptionOccurred = true;
+    }
+    expect(exceptionOccurred, isTrue);
+  });
 }
