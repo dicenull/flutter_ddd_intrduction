222553d6392a4441e9b50cf0b8140b0b40bb5d16
dicenull <dicenull.dev@gmail.com>
2025-07-10 19:21:23 +0900

    No.3186 ユーザ登録テスト

---
+++ test/sns/user_register_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_ddd_introduction/sns/in_memory_user_repository.dart';
+import 'package:flutter_ddd_introduction/sns/program.dart';
+import 'package:flutter_ddd_introduction/sns/user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'create_container.dart';
+
+void main() {
+  group('ユーザ登録', () {
+    test('ユーザが最小の文字列で登録できる', () {
+      final container = createContainer(
+        overrides: [
+          userRepositoryPod.overrideWithValue(InMemoryUserRepository()),
+        ],
+      );
+
+      final userRepository = container.read(userRepositoryPod);
+      final userApplicationService = container.read(userApplicationServicePod);
+
+      container.listen(userApplicationServicePod, (_, __) {});
+
+      userApplicationService.register("123");
+
+      // assert
+      final user = userRepository.findByName(UserName("123"));
+      expect(user, isNotNull);
+    });
+  });
+}
